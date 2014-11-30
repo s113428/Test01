6d24acd08f7906c7ab47c6a91bad8addb75caf61
s113428 <fujigithub@gmail.com>
2014-11-30 09:34:35 +0900

    study DataBase on TestDB branch

--- app/src/main/java/com/test/test01/MainActivity.java
+++ app/src/main/java/com/test/test01/MainActivity.java
@@ -1,9 +1,16 @@
 package com.test.test01;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,82 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyOpenHelper helper = new MyOpenHelper(this);
+        final SQLiteDatabase db = helper.getWritableDatabase();
+
+        final EditText nameText = (EditText) findViewById(R.id.editName);
+        final EditText ageText = (EditText) findViewById(R.id.editAge);
+
+        Button entryButton = (Button) findViewById(R.id.insert);
+        entryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameText.getText().toString();
+                String age= ageText.getText().toString();
+
+                ContentValues insertValues = new ContentValues();
+                insertValues.put("name",name);
+                insertValues.put("age",age);
+                long id = db.insert("person", name, insertValues);
+            }
+        });
+
+        Button updataButton = (Button) findViewById(R.id.update);
+        updataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameText.getText().toString();
+                String age = ageText.getText().toString();
+
+                if (name.equals("")){
+                    Toast.makeText(MainActivity.this,"名前を入力して下さい。",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    ContentValues updateValues = new ContentValues();
+                    updateValues.put("age",age);
+                    db.update("person", updateValues, "name=?", new String[] { name });
+                }
+            }
+        });
+
+        Button deleteButton = (Button) findViewById(R.id.delete);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameText.getText().toString();
+                String age = ageText.getText().toString();
+
+                if (name.equals("")){
+                    Toast.makeText(MainActivity.this, "名前を入力して下さい。",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    db.delete("person", "name=?", new String[] { name });
+                }
+            }
+        });
+
+        Button deleteAllButton = (Button) findViewById(R.id.deleteAll);
+        deleteAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameText.getText().toString();
+                String age = ageText.getText().toString();
+
+                db.delete("person", null, null);
+
+            }
+        });
+
+        Button dataBaseButton = (Button) findViewById(R.id.dataBase);
+        dataBaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dbIntent = new Intent(MainActivity.this, ShowDataBase.class);
+                startActivity(dbIntent);
+            }
+        });
+
     }
 
 
@@ -36,4 +119,7 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
